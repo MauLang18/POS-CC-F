ed9f45c846803bd5a0cf4ec31bf0d1c54ccca27e
MauLang18 <maulangbonilla.18@gmail.com>
2024-11-24 12:26:11 -0600

    add ci

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   environment: "local",
   clientId:
     "441710587933-o28p6ici53uclbea0rjveo6gcdvkuaes.apps.googleusercontent.com",
-  api: "http://localhost:5011/api/",
+  api: "https://api.customcodecr.com/api/",
   filenameCsv: "dev",
 };
